7f9389cb9c6b98f67478f560ba0056f42341cfe1
김윤수 <yskim@taeha.co.kr>
2020-06-17 14:54:37 +0900

    중복 사용자 체크 함수 추가

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -15,17 +15,30 @@
         }
 
         /* CRUD -> C -> CREATE */
-        function createUser($username, $pass, $email, &$errmsg) {
-            $password = md5($pass);
-            $stmt = $this->con->prepare("INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, ?, ?, ?);");
-            $stmt->bind_param("sss", $username, $password, $email);
-
-            if($stmt->execute()) {
-                $errmsg = "User registered successfully";
-                return true;
-            } else {
-                $errmsg = $stmt->error;
+        public function createUser($username, $pass, $email, &$errmsg) {
+            if($this->isUserExist($username, $email)) {
+                $errmsg = "It seems you are already registered, please choose a different email and username";
                 return false;
-            }
+            } else {
+                $password = md5($pass);
+                $stmt = $this->con->prepare("INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, ?, ?, ?);");
+                $stmt->bind_param("sss", $username, $password, $email);
+
+                if($stmt->execute()) {
+                    $errmsg = "User registered successfully";
+                    return true;
+                } else {
+                    $errmsg = $stmt->error;
+                    return false;
+                }
+            }            
+        }
+
+        private function isUserExist($username, $email) {
+            $stmt = $this->con->prepare("SELECT id From users WHERE username = ? OR email = ?");
+            $stmt->bind_param("ss", $username, $email);
+            $stmt->execute();
+            $stmt->store_result();  // mysqli 버퍼에 담긴 값을 php 변수에 복사한다.
+            return $stmt->num_rows > 0;
         }
     }
